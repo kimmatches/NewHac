3dff89760fb14bbae3278d36b6d498a279977a38
user <peach3139@naver.com>
2023-09-01 20:54:10 +0900

    새로고침

--- src/App.js
+++ src/App.js
@@ -1,105 +1,12 @@
-// import { useState } from "react";
-// import "./styles.css";
-// import MyCalender from "./MyCalender";
-//
-//
-// function addMessage(message, sender) {
-//   const chatMessagesElement = document.getElementById("chat-messages");
-//   const messageElement = document.createElement("div");
-//   messageElement.classList.add("chat-message", sender);
-//   messageElement.textContent = message;
-//   chatMessagesElement.appendChild(messageElement);
-//   chatMessagesElement.scrollTop = chatMessagesElement.scrollHeight; // Scroll to bottom
-// }
-//
-//
-//
-// function simulateBotResponse(userInput) {
-//   // 여기서 실제 GPT 모델과의 상호작용을 시뮬레이션할 수 있습니다.
-//   const botResponse = `당신이 말한 내용: "${userInput}" 에 대한 대답입니다.`;
-//   setTimeout(() => {
-//     addMessage(botResponse, "bot");
-//   }, 500); // 임의로 0.5초 후에 응답 추가
-// }
-//
-// export default function App() {
-//   const [text,setText] = useState("");
-//
-//   const onChange = (e) => {
-//     setText(e.target.value);
-//   }
-//   const onReset = () => {
-//     setText("");
-//   }
-//
-//   const sendChat = () => {
-//     addMessage(text, "user");
-//     simulateBotResponse(text);
-//     onReset();
-//   }
-// //   function App() {
-// //   return (
-// //       <div className="App">
-// //         <MyCalender />
-// //       </div>
-// //   );
-// // }
-//
-//   // const handleOnKeyPress = (e) => {
-//   //   if (e.key === "Enter") {
-//   //     sendChat(); // Enter 입력이 되면 클릭 이벤트 실행
-//   //   }
-//   // };
-//
-//   return (
-//     <>
-//     <body>
-//       <title>ChatGPT 웹사이트 예제</title>
-//       <nav className="sidebar">
-//         <ul className="navbar">
-//           <li>
-//             <a href={1}>Chat</a>
-//           </li>
-//           <li>
-//             <a href={2}>Calendar</a>
-//           </li>
-//         </ul>
-//       </nav>
-//       {/*<div className="chat-container" id="">*/}
-//       {/*  <div className="chat">*/}
-//       {/*    <div className="chat-messages" id="chat-messages">*/}
-//       {/*      <div className="chat-message bot">*/}
-//       {/*        안녕하세요! 무엇을 도와드릴까요?*/}
-//       {/*      </div>*/}
-//       {/*    </div>*/}
-//       {/*    <input*/}
-//       {/*      type="text"*/}
-//       {/*      id="user-input"*/}
-//       {/*      placeholder="메시지를 입력하세요..."*/}
-//       {/*      onChange={onChange}*/}
-//       {/*    />*/}
-//       {/*    <button type="button" onClick={sendChat}>*/}
-//       {/*      {" "}*/}
-//       {/*      확인*/}
-//       {/*    </button>*/}
-//       {/*  </div>*/}
-//       {/*</div>*/}
-//      <div className="App">
-//         <MyCalender />
-//       </div>
-//       </body>
-//     </>
-//
-//
-//   );
-// }
-
-import { useState } from "react";
-// import "./styles.css";
 import './App.css';
 import MyCalendar from "./MyCalender";
 
 function App() {
+
+    const events = [
+        { title: '재관 만나기', date: '2023-09-01' },
+        { title: '민석 만나기', date: '2023-09-22' }
+    ];
   return (
       <>
           <nav className="sidebar">
@@ -114,7 +21,8 @@ function App() {
       </nav>
 
     <div className="App">
-      <MyCalendar />
+      <MyCalendar events={events}/>
+        {/*<MyCalendar />*/}
     </div>
     </>
   );
--- src/MyCalender.js
+++ src/MyCalender.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import './MyCalender.css'
+import * as events from "events";
 class MyCalendar extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    dateClick=(info)=>{
+    const clickedDate = info.dateStr;
+    const clickedEvent = this.props.events.find(event => event.date === clickedDate);
+
+    if (clickedEvent) {
+        alert(`일정 : ${clickedEvent.title}\n${clickedEvent.date}`);
+    } else {
+        alert(info.dateStr);
+    }
+}
     render() {
         return (
           <div className="App">
+              {/*<div style={{ margin:10, display:'grid',gridTemplateColumns:"2fr 1fr"}}>*/}
             <FullCalendar
-                defaultView="dayGridMonth"
-                plugins={[ dayGridPlugin ]}
-                events={[
-                    { title: 'event 1', date: '2023-09-01' },
-                    { title: 'event 2', date: '2023-09-02' }
-                ]}
+                 plugins={[dayGridPlugin,timeGridPlugin,interactionPlugin]}
+                    initialView={'dayGridMonth'}
+                    headerToolbar={
+                        {
+                            start: 'today',
+                            center: 'title',
+                            end: 'prev,next'
+                        }
+                    }
+                    height={"100vh"}
+                dateClick = {this.dateClick}
+                 events={this.props.events}
+                // events={[
+                //     { title: 'event 1', date: '2023-09-01' },
+                //     { title: 'event 2', date: '2023-09-02' }
+                // ]}
             />
+              {/*</div>*/}
           </div>
         );
     }
